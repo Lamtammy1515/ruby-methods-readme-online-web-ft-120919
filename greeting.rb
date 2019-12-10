8d6445bf007fd0e90bdaaf709aa0b84afc67d957
Tammy Lam <lamtammy1515@gmail.com>
2019-12-10 02:03:38 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,6 +1,9 @@
-// ♥ ruby greeting.rb
-Hello World!
-Hello World!
-Hello World!
-Hello World!
-Hello World!
+def greeting
+  puts "Hello World!"
+end
+
+greeting 
+greeting
+greeting
+greeting
+greeting
